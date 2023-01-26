580fa2c5fbf8bfbb5d1c3138fd5c04be00e7c565
isfloresv <isaacfloresv@gmail.com>
2023-01-26 13:59:20 -0600

    Change en DB

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('id20145964_meic_web', 'id20145964_alega_meic_web','e$ks3d~Fg1b&d%TW', {
-    host: 'localhost',
+const db = new Sequelize('ptscmeicweb', 'g2xy5ksk5l0156b7q9ez','pscale_pw_5EApJQKSbOW6iPRhS2UBvTHJG8gIysP2jMMezqhh4iL', {
+    host: 'us-east.connect.psdb.cloud',
     dialect: 'mysql'
     }
 )
