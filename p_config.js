03ee86c322737b5d74817c38b783e7e725f0de2e
isfloresv <isaacfloresv@gmail.com>
2023-01-30 15:09:14 -0600

    Cambios en packge

--- p_config.js
+++ p_config.js
@@ -3,11 +3,11 @@ import {config} from 'dotenv'
 config()
 
 export const APP_PORT =  process.env.PORT || 4002
-export const DB_USER =  process.env.DB_USER || 'meicapp'
-export const DB_PASSWORD =  process.env.DB_PASSWORD ||'appmeicno023'
-export const DB_HOST =  process.env.DB_HOST || '127.0.0.1'
+export const DB_USER =  process.env.DB_USER || 'root'
+export const DB_PASSWORD =  process.env.DB_PASSWORD ||'RVpYPQoB6LWZfoy9jef2'
+export const DB_HOST =  process.env.DB_HOST || 'containers-us-west-18.railway.app'
 export const DB_DATABASE =  process.env.DB_DATABASE || 'meic'
-export const DB_PORT =  process.env.DB_PORT ||3306
+export const DB_PORT =  process.env.DB_PORT || 6197
 
 
 
