2e564979398570fd61ce4a20b0ffb8032b99f525
isfloresv <isaacfloresv@gmail.com>
2023-03-01 11:29:25 -0600

    change in Controllers MAt

--- controllers/MatController.js
+++ controllers/MatController.js
@@ -16,7 +16,7 @@ export const getAllMat = async (req, res) => {
 export const getMat = async (req, res) => {
     try {
         const materia = await MatModel.findAll({
-            where:{id:req.params.id_materia}
+            where:{ident:req.params.id_materia}
         })
         res.json(materia)
     } catch (error) {
@@ -41,7 +41,7 @@ export const createMat = async(req, res) => {
 export const updateMat = async(req, res) => {
     try {
         MatModel.update(req.body, {
-            where: {id: req.params.id_materia}
+            where: {ident: req.params.id_materia}
         })
     } catch (error) {
         res.json({
