89db98e9bd6bbbc676d9d5f4c3f96598fce7453c
isfloresv <isaacfloresv@gmail.com>
2023-03-28 13:37:30 -0600

    Modified filter

--- controllers/EstadistidController.js
+++ controllers/EstadistidController.js
@@ -5,14 +5,20 @@ import db from "../database/db.js";
 
 export const getTopElem = async (req, res) => {
     try {
+        const query1 = `SELECT ` + req.body.elemt + ` AS elemt, COUNT(` + req.body.elemt + `) AS total FROM asesopreses
+        GROUP BY `+ req.body.elemt + ` ORDER BY ` + req.body.elemt + ` DESC LIMIT ` + req.body.top + ``
+        const query2 = `SELECT * FROM asesopreses 
+        where (STR_TO_DATE(SUBSTRING_INDEX(fchareg, ' ', 1),'%e/%m/%Y') >= ` + req?.body.fchaIni + ` 
+        AND STR_TO_DATE(SUBSTRING_INDEX(fchareg, ' ', 1),'%e/%m/%Y') <= ` + req?.body.fchaFin + `)`
         console.log(req.body)
-        if(req.body.opc = 1){
-        const dato = await db.query(
-        `SELECT `+req.body.elemt+` AS elemt, COUNT(`+req.body.elemt+`) AS total FROM asesopreses
-        GROUP BY `+req.body.elemt+` ORDER BY `+req.body.elemt+` DESC LIMIT `+req.body.top+``)
-        res.json(dato)
-    }else{res.send("no hay resultados "+req.body )}
+        if (req.body.opc = 1) {
+            const dato = await db.query(query1)
+            res.json(dato)
+        } else if (req.body.opc = 2) {
+            const dato = await db.query(query2)
+            res.json(dato)
+        } else { res.send("no hay resultados " + req.body) }
     } catch (error) {
-        res.json({message: error.message})
+        res.json({ message: error.message })
     }
 }
