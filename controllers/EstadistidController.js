b55197360126bed8274495655b5d6bdd2fb12539
isfloresv <isaacfloresv@gmail.com>
2023-04-11 02:37:18 -0600

    Modified estadistic

--- controllers/EstadistidController.js
+++ controllers/EstadistidController.js
@@ -6,10 +6,12 @@ import db from "../database/db.js";
 export const getTopElem = async (req, res) => {
     try {
         const query1 = `SELECT ` + req.body.elemt + ` AS elemt, COUNT(` + req.body.elemt + `) AS total FROM asesopreses
+        where (STR_TO_DATE(SUBSTRING_INDEX(fchareg, ' ', 1),'%e/%m/%Y') >= ` + req.body.fchaIni + ` 
+        AND STR_TO_DATE(SUBSTRING_INDEX(fchareg, ' ', 1),'%e/%m/%Y') <= ` + req.body.fchaFin + `)
         GROUP BY `+ req.body.elemt + ` ORDER BY ` + req.body.elemt + ` DESC LIMIT ` + req.body.top + ``
         const query2 = `SELECT * FROM asesopreses 
-        where (STR_TO_DATE(SUBSTRING_INDEX(fchareg, ' ', 1),'%e/%m/%Y') >= ` + req?.body.fchaIni + ` 
-        AND STR_TO_DATE(SUBSTRING_INDEX(fchareg, ' ', 1),'%e/%m/%Y') <= ` + req?.body.fchaFin + `)`
+        where (STR_TO_DATE(SUBSTRING_INDEX(fchareg, ' ', 1),'%e/%m/%Y') >= ` + req.body.fchaIni + ` 
+        AND STR_TO_DATE(SUBSTRING_INDEX(fchareg, ' ', 1),'%e/%m/%Y') <= ` + req.body.fchaFin + `)`
         console.log(req.body)
         if (req.body.opc = 1) {
             const dato = await db.query(query1)
